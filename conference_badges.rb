084b591890eb5945d75f03107fb7a98adb6516ed
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-01-29 19:42:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+ attendees.each do |names| 
+puts "Hello, my name is #{names}."
+
+end
+end
